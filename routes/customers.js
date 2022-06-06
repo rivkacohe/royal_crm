36632223f40fcc93e44222086ad053e09d45274d
rivka <rarivkale@gmail.com>
2022-06-06 10:39:17 +0300

    aa

--- routes/customers.js
+++ routes/customers.js
@@ -7,11 +7,16 @@ const fileMngmt = require('../shared/fileMngmt')
 
 /* customers-home */
 router.get('/home', function(req, res, next){
-    const filePath = fileMngmt.getHtmlFilePath('customers-home.html'); 
+    const filePath = fileMgmt.getHtmlFilePath('customers-home.html'); 
     // c:\prjects\royal-crm\client\customers-home.html
     res.sendFile(filePath);
   })
-
+  
+// http://localhost:3000/customers/details/2
+router.get('/details/:id', function (req, res, next) {
+  const filePath = fileMgmt.getHtmlFilePath('customer-details.html');
+  res.sendFile(filePath);
+});
   
   router.get   ('/', cm.customersList);
   router.get   ('/find', cm.findCustomer);
