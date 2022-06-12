285344747da7a2626997d27848349532e8d493a8
rivka <rarivkale@gmail.com>
2022-06-12 21:17:27 +0300

    פרשבאןבק אפ

--- app.ts
+++ app.ts
@@ -1,3 +1,5 @@
+import { Application,Request,Response,NextFunction,ErrorRequestHandler } from "express";
+
 //var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
